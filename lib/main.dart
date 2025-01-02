3f30f9e4ece0d36c36da7a2a2ea1c08e5548a2eb
EmanT <tharwateman38@gmail.com>
2025-01-02 02:15:46 +0200

    --

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:e_commerce/core/bloc/bloc_observer.dart';
 import 'package:e_commerce/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(MyApp());
 }
